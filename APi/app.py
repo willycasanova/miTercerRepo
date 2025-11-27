59395e0edb57d8cdfdbc7161514314780adcde74
Willy <willycasanova424@gmail.com>
2025-11-27 16:37:52 +0100

    metodo platzi

--- APi/app.py
+++ APi/app.py
@@ -34,3 +34,9 @@ def get_starwars_warriors():
 def get_warriors_got():
     rows = ["Jon Snow", "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Cersei Lannister", "Jaime Lannister", "Bran Stark", "Sansa Stark"]
     return rows
+
+
+@app.get("/cursosPlatzi")
+def get_cursos():    
+    rows = ["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"]    
+    return rows
